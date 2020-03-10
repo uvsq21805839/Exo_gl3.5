2bfc119b12cb267a5a9556bf6d3a8746d77b1c2e
21805839 <dskprod@gmail.com>
2020-03-10 19:51:26 +0100

    test unitaire

--- src/test/java/uvsq/Exo_gl3/AppTest.java
+++ src/test/java/uvsq/Exo_gl3/AppTest.java
@@ -1,38 +1,23 @@
 package uvsq.Exo_gl3;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+
+
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+	UneClasseMetier metier ;
+   @Test
     public void testApp()
     {
-        assertTrue( true );
+	   metier=new UneClasseMetier(new Message());
+	   metier.intruction();
     }
 }
