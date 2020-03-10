b6723dc0ce538ee1b5756336e18d1da4e37b7925
21805839 <dskprod@gmail.com>
2020-03-10 18:57:56 +0100

    repository

---
+++ src/main/java/uvsq/Exo_gl3/App.java
@@ -0,0 +1,13 @@
+package uvsq.Exo_gl3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
